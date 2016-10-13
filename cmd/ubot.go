384e90a5d7340f0fd616460d981456688ab0ed1e
Jun HU <jun.hu@ubisoft.com>
2016-10-13 14:54:17 -0700

    add certificate for gRPC

--- cmd/ubot.go
+++ cmd/ubot.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	//"fmt"
+	"flag"
+	"fmt"
 	"log"
 	"net"
 	"os/exec"
@@ -9,10 +10,19 @@ import (
 	pb "github.com/perigee/ubot/pb"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 const (
 	port = ":50051"
+)
+
+var (
+	tls        = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+	certFile   = flag.String("cert_file", "testdata/server1.pem", "The TLS cert file")
+	keyFile    = flag.String("key_file", "testdata/server1.key", "The TLS key file")
+	jsonDBFile = flag.String("json_db_file", "testdata/route_guide_db.json", "A json file containing a list of features")
+	port       = flag.Int("port", 50051, "The server port")
 )
 
 type server struct{}
@@ -33,19 +43,29 @@ func (s *server) GetInfo(ctx context.Context, in *pb.InfoRequest) (*pb.InfoRespo
 
 func main() {
 
-     creds, err := credentials.NewServerTLSFromFile(certFile, keyFile)
+	flag.Parse()
 
-     if err != nil {
-     	log.Fatalf("Failed to generate creds: %v", err)
-     }
-     
-	lis, err := net.Listen("tcp", port)
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 
 	if err != nil {
 		log.Fatalf("Failed: %v", err)
 	}
 
-	s := grpc.NewServer(grpc.Creds(creds))
+	var opts []grpc.ServerOption
+
+	if *tls {
+
+		creds, err := credentials.NewServerTLSFromFile(*cert_file, *key_file)
+
+		if err != nil {
+			log.Fatalf("Failed to generate creds: %v", err)
+		}
+
+		opts = []grpc.ServerOption{grpc.Creds(creds)}
+
+	}
+
+	s := grpc.NewServer(opts...)
 	pb.RegisterInfoServer(s, &server{})
 	s.Serve(lis)
 }
