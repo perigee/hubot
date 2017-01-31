3b6cfcb4ee6a48a3eba0cdf722082871e803c959
Jun HU <jun.hu@ubisoft.com>
2017-01-31 22:53:16 +0000

    init dispatcher

---
+++ dispatcher.go
@@ -0,0 +1,112 @@
+package ubot
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var (
+	MaxWorker = os.Getenv("MAX_WORKERS")
+	MaxQueue  = os.Getenv("MAX_QUEUE")
+)
+
+// Job represents the job to be run
+type Job struct {
+	Payload string
+}
+
+// A buffered channel that we can send work requests on.
+var JobQueue chan Job
+
+// Worker represents the worker that executes the job
+type Worker struct {
+	WorkerPool chan chan Job
+	JobChannel chan Job
+	quit       chan bool
+}
+
+func NewWorker(workerPool chan chan Job) Worker {
+	return Worker{
+		WorkerPool: workerPool,
+		JobChannel: make(chan Job),
+		quit:       make(chan bool)}
+}
+
+// Start method starts the run loop for the worker, listening for a quit channel in
+// case we need to stop it
+func (w Worker) Start() {
+	go func() {
+		for {
+			// register the current worker into the worker queue.
+			w.WorkerPool <- w.JobChannel
+
+			select {
+			case job := <-w.JobChannel:
+				// we have received a work request.
+				if err := job.Payload.UploadToS3(); err != nil {
+					log.Errorf("Error uploading to S3: %s", err.Error())
+				}
+
+			case <-w.quit:
+				// we have received a signal to stop
+				return
+			}
+		}
+	}()
+}
+
+// Stop signals the worker to stop listening for work requests.
+func (w Worker) Stop() {
+	go func() {
+		w.quit <- true
+	}()
+}
+
+type Dispatcher struct {
+	MaxWorkers int
+	// A pool of workers channels that are registered with the dispatcher
+	WorkerPool chan chan Job
+}
+
+func NewDispatcher(maxWorkers int) *Dispatcher {
+	pool := make(chan chan Job, maxWorkers)
+
+	maxWorker, err := strconv.ParseInt(MaxWorker, 10, 16)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return &Dispatcher{
+		MaxWorkers: int(maxWorker),
+		WorkerPool: pool,
+	}
+}
+
+func (d *Dispatcher) Run() {
+	// starting n number of workers
+	for i := 0; i < d.MaxWorkers; i++ {
+		worker := NewWorker(d.WorkerPool)
+		worker.Start()
+	}
+
+	go d.dispatch()
+}
+
+func (d *Dispatcher) dispatch() {
+	for {
+		select {
+		case job := <-JobQueue:
+			// a job request has been received
+			go func(job Job) {
+				// try to obtain a worker job channel that is available.
+				// this will block until a worker is idle
+				jobChannel := <-d.WorkerPool
+
+				// dispatch the job to the worker job channel
+				jobChannel <- job
+			}(job)
+		}
+	}
+}
